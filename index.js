2be3dfe0a037c244c3f93fa9f8162fd0d9223711
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2024-10-16 22:31:24 +0530

    JS file added

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log(a)
+// var a=6;
+// var abc="matriculation";
+// console.log(a+"after")
+// var ar=[1,2,3,5,6,8,9,3,2,1];
+// let obj={};
+// for(var i=0;i<ar.length;i++){
+//     if(obj[ar[i]]==undefined){
+//         obj[ar[i]]=1;
+//     }
+//     else{
+//        obj[ar[i]]=obj[ar[i]]+1
+//     }
+// }
+
+// console.log(obj);
+console.log(a)
+
+console.log(1-"1")
+const c=89;
+console.log(b)
+console.log(a)
+console.log(c)
+
+
+// object can be accessed using dot . or bracket []. Eg- obj.a ,obj["a"]
+// typeof - is used to find the typeof variable
+// typeof array is - object
