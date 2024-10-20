b2b119cd1989d98b300b7014e18d9e8a72ed77dd
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2024-10-20 22:00:08 +0530

    Input console assignment added

--- index.js
+++ index.js
@@ -14,15 +14,29 @@
 // }
 
 // console.log(obj);
-console.log(a)
+// console.log(a)
 
-console.log(1-"1")
-const c=89;
-console.log(b)
-console.log(a)
-console.log(c)
+// console.log(1-"1")
+// const c=89;
+// console.log(b)
+// console.log(a)
+// console.log(c)
 
 
 // object can be accessed using dot . or bracket []. Eg- obj.a ,obj["a"]
 // typeof - is used to find the typeof variable
 // typeof array is - object
+
+
+
+let arr=[1,2,3,4,5,6,7,8];
+// arr.splice(1,1)
+// a = arr.slice(2,5)
+// arr.push("Priyanshu")
+// a = arr.pop()
+// a = arr.unshift("Priyanshu")
+// a = arr.shift(0)
+
+
+// console.log(arr)
+console.log(a,arr)
