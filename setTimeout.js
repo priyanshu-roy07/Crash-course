f1270b8ed3879b256268b4569031794bda5b8858
priyanshu-roy07 <156593208+priyanshu-roy07@users.noreply.github.com>
2024-11-13 23:14:15 +0530

    new assignment added

---
+++ setTimeout.js
@@ -0,0 +1,35 @@
+// let arr=[
+//     {name:"xyz",age:89,address:"Patna"},
+//     {name:"xyz1",age:39,address:"Patna"},
+//     {name:"xyz2",age:59,address:"delhi"},
+//     {name:"xyz3",age:69,address:"delhi"},
+//     {name:"asd2",age:29,address:"Patna"},
+//     {name:"asd1",age:29,address:"Patna"}
+// ]
+
+//Calculate avg of all age if age >30.
+//Count how many are from patna.
+// build one rectangle bg red color. after 10sec convert in cirle and in blue color.
+// build a timer when a button is click timer starts.
+
+let total=0;
+let c=0;
+console.log(5);
+
+setTimeout(function(){
+    clearInterval(a)
+},10000)
+let c1=0
+ let a=setInterval(function(){
+    c1++
+    document.getElementById('k').innerText=c1
+},1000)
+
+
+console.log(c)
+console.log(4)
+for(let i=0;i<100000;i++){
+   
+}
+
+console.log(4)
